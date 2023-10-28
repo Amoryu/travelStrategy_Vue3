1d9327a4d2b5b521d07d903fbe552e86ede2084f
Amor yu <2014377205@qq.com>
2023-10-28 23:43:38 +0800

    项目初始化

---
+++ App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Index/>
+</template>
+<script setup>
+import Index from './pages/index/index.vue'
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ main.js
@@ -0,0 +1,12 @@
+import App from './App'
+import { createSSRApp } from 'vue'
+// import PageHead from './components/page-head.vue' //全局引用 page-head 组件
+
+export function createApp() {
+    const app = createSSRApp(App)
+    // app.component('page-head', PageHead) //全局注册 page-head 组件，每个页面将可以直接使用该组件
+
+    return {
+        app
+    }
+}
